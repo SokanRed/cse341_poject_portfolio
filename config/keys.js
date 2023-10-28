a55c6bbd899ca760d2602354e7050e4fe8502443
Denis PC Poussard <denis@sokan.fr>
2023-10-28 21:06:56 +0200

    Begin implementing a login system in the application

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+//  add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '1006629722611-6q80d7uee2e1hv5fkb8mnrqiec0ief9h.apps.googleusercontent.com',
+        clientSecret: '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'
+    },
+    mongodb: {
+        dbURI: 'mongodb+srv://dernakos:Sp3gno24Lini@cluster0.cpiccp7.mongodb.net/?retryWrites=true&w=majority'
+    }
+};
--- server.js
+++ server.js
@@ -1,16 +1,46 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 // const expressValidator = require('express-validator');
 const mongodb = require('./db/connect');
+
 // Import the cors middleware
 const cors = require('cors');
+const passport = require('passport');
+const authRoutes = require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
+require('./config/passport-setup')(passport);
+
+app.set('view engine', 'ejs');
+
+//  connect to mongodb
+mongoose.connect(keys.mongodb.dbURI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(error => {
+        console.error('Error connecting to MongoDB: ' + error);
+    });
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
 app
     .use(bodyParser.json())
-    // rs.use(expressValidator)
+    .use(session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false
+    }))
+    .use(passport.initialize())
+    .use(passport.authenticate('session'))
+    .use('/auth', authRoutes)
     .use(cors({ origin: '*' }))
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
