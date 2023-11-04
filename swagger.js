8a774afec626958b6d8579696c448285afef86cc
Denis PC Poussard <denis@sokan.fr>
2023-11-04 21:30:22 +0100

    Error 404

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'My API',
         description: 'Description',
     },
-    host: 'localhost:8080',
+    host: 'sokanred341-project-portfolio.onrender.com',
     schemes: ['https'],
 };
 
